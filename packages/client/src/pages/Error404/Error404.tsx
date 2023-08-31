82876f542b1bd7f42b3359d1d9ac8e6463de9b52
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-08-31 15:04:06 +0300

    Init

--- packages/client/src/pages/Error404/Error404.tsx
+++ packages/client/src/pages/Error404/Error404.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import './style.scss'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Error404: React.FC = function () {
-  return <div>Error404</div>
+  const navigate = useNavigate()
+
+  function goHome() {
+    navigate('/')
+  }
+
+  return (
+    <div className="page404">
+      <div className="page404__statusBlock">Ошибка: 404</div>
+
+      <div className="page404__content">
+        <div className="page404__content__description">
+          <div className="page404__content__description__title">Упс!)</div>
+          <div className="page404__content__description__text">
+            Такой страницы не существует.
+          </div>
+        </div>
+
+        <div onClick={goHome} className="page404__content__homeLink">
+          Можете вернуться
+          <br />
+          на <Link to="/">гланую страницу</Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export { Error404 }
