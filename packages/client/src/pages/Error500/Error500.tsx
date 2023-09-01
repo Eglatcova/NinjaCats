0c72620289a7ffd881a34719f5164b0407802554
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-09-01 13:55:03 +0300

    Finish

--- packages/client/src/pages/Error500/Error500.tsx
+++ packages/client/src/pages/Error500/Error500.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import './style.scss'
 
 const Error500: React.FC = function () {
-  return <div>Error500</div>
+  return (
+    <div className="page500">
+      <div className="page500__statusBlock">Ошибка: 500</div>
+
+      <div className="page500__content">
+        <div className="page500__content__description">
+          <div className="page500__content__description__title">Упс!)</div>
+          <div className="page500__content__description__text">
+            Произошла ошибка на сервере.
+          </div>
+          <div className="page500__content__description__text">
+            Мы уже работаем над ней.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export { Error500 }
