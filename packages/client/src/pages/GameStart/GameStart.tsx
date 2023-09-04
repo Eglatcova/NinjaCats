34b4721505a319d6089c8b50ca4c5303e6850423
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-09-04 19:34:30 +0300

    Init

--- packages/client/src/pages/GameStart/GameStart.tsx
+++ packages/client/src/pages/GameStart/GameStart.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './style.scss'
+import { useNavigate } from 'react-router-dom'
 
 const GameStart: React.FC = function () {
-  return <div>GameStart</div>
+  const navigate = useNavigate()
+
+  function startGame() {
+    navigate('/game')
+  }
+
+  return (
+    <div className="GameStart">
+      <div onClick={startGame} className="GameStart__button">
+        Начать
+      </div>
+    </div>
+  )
 }
 
 export { GameStart }
