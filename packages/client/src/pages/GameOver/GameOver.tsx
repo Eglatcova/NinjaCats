a2ca449ecf7704a7aa1e9c47617e704bd1ce5882
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-09-04 19:49:39 +0300

    Init

--- packages/client/src/pages/GameOver/GameOver.tsx
+++ packages/client/src/pages/GameOver/GameOver.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import './style.scss'
+import React, { useEffect } from 'react'
+// import { authController } from '../../controllers/AuthController'
+import { useNavigate } from 'react-router-dom'
 
 const GameOver: React.FC = function () {
-  return <div>GameOver</div>
+  const navigate = useNavigate()
+
+  function tryAgain() {
+    navigate('/game')
+  }
+
+  // useEffect(() => {
+  //   authController.getUser().then(res => {
+  //     if (!res) {
+  //       navigate('/')
+  //     }
+  //   })
+  // }, [])
+
+  return (
+    <div className="GameOver">
+      <div onClick={tryAgain} className="GameOver__button">
+        Попробывать снова
+      </div>
+    </div>
+  )
 }
 
 export { GameOver }
