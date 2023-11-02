37568eb3cfe05618be335e9c9d0d50dd1588758f
Angry Philomel <an.aries@icloud.com>
2023-11-02 12:29:46 +0300

    fix: evn loader for ssr

--- packages/client/ssr.config.ts
+++ packages/client/ssr.config.ts
@@ -1,33 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 import svgr from 'vite-plugin-svgr'
 import * as path from 'path'
 
-dotenv.config()
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve('../../'), '')
 
-export default defineConfig({
-  define: {
-    __REDIRECT_URI__: process.env.REDIRECT_URI,
-  },
-  plugins: [svgr(), react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/style/variables";`,
+  return {
+    define: {
+      __REDIRECT_URI__: JSON.stringify(env.REDIRECT_URI),
+    },
+    plugins: [svgr(), react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/style/variables";`,
+        },
       },
     },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'ssr.tsx'),
-      name: 'Client',
-      formats: ['cjs'],
-    },
-    rollupOptions: {
-      output: {
-        dir: 'ssr-dist',
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'ssr.tsx'),
+        name: 'Client',
+        formats: ['cjs'],
+      },
+      rollupOptions: {
+        output: {
+          dir: 'ssr-dist',
+        },
       },
     },
-  },
+  }
 })
